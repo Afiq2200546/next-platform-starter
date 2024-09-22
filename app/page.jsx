cd634ccac3727a45c85a29598172055ceb4405b2
Afiq2200546 <115019647+Afiq2200546@users.noreply.github.com>
2024-09-22 19:25:05 +0800

    test

--- app/page.jsx
+++ app/page.jsx
@@ -34,6 +34,7 @@ export default function Page() {
                 <ContextAlert />
                 <h1 className="mb-0">Netlify Platform Starter - Next.js</h1>
                 <p className="text-lg">Get started with Next.js and Netlify in seconds.</p>
+                <p className="text-lg">TESSSSSSSSSSSSSSSSSSSSSST</p>
                 <Link
                     href="https://docs.netlify.com/frameworks/next-js/overview/"
                     className="btn btn-lg btn-primary sm:btn-wide"
